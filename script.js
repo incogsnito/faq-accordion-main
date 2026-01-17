25b6647eae8b1a51578604ef52e5a97dc955bc74
Samad <Samad.o@outlook.com>
2026-01-16 23:19:22 -0500

    project complete

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-
 document.addEventListener("click", (e) => {
-  if (!e.target.matches(".add")) return;
+  if (!e.target.matches(".add, .minus")) return;
 
   const container = e.target.closest(".info_container");
   const info = container.querySelector(".info");
+  const plus = container.querySelector(".add");
+  const minus = container.querySelector(".minus");
 
   info.classList.toggle("hidden");
+  minus.classList.toggle("hidden");
+  plus.classList.toggle("hidden");
 });
